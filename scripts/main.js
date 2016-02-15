aa6ff7a93326501899f42fa6f4841e0d921e64ca
jvmqueue <wheels0090@yahoo.com>
2016-02-15 18:43:29 -0500

    main calls getData once DOM is available. We do not need jQuery to discover DOM ready, optimization

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
-require(['homeView', 'configMapping'], 
-  function(homeView, configMapping, undefined){
+require(['getData'], 
+  function(getData, undefined){
     'use strict';
     var w = window, d = document;
 
     var interval = w.setInterval(function(){ // we don't need jQuery to wait for DOM
-    if(d.getElementsByTagName('div').length > 1){
+    if(d.getElementsByTagName('div').length > 0){
       w.clearInterval(interval);
-      var view = new homeView.View(configMapping.mapping.viewToData);
+      getData.fnc.test();
     }
     }, 33);
 
-}); // End require(['jQuery', 'Backbone'] ...
+}); // End require([] ...
 
 
 
