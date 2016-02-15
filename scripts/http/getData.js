24d7ce20ce77a385a1c5584646ff8c04c0be2c68
jvmqueue <wheels0090@yahoo.com>
2016-02-15 18:49:41 -0500

    AMD files

---
+++ scripts/http/getData.js
@@ -0,0 +1,98 @@
+/* https://www.sitepoint.com/community/t/recursive-function-to-loop-through-xml-dom/3293 */
+
+define(['jQuery', 'util'], function($, util, undefined){
+    'use strict';
+    var mIntCounter = 0;
+    var mData = null;
+    var mNodeDocument = null;
+    var mArrayNodes = [];
+    var mHashText = {_text:{}};
+    var mTEXT_NODE = 3;
+    var mELEMENT_NODE = 1;
+    var mDOCUMENT_NODE = 9;
+
+
+var getDocumentElement = function(e, data) {
+
+    //
+    //  Output function.
+    //
+    var o = {
+        _text: '',
+    };
+
+    //
+    //  c = each child
+    //
+    var c = data.firstChild;
+
+    //
+    //  Loop through child nodes.
+    //
+    while (c) {
+
+        //
+        //  Add to _text property if it is a text.
+        //
+        if (c.nodeName == '#text') {
+            o._text += c.nodeValue;
+
+        //
+        //  Else if it is a normal node.
+        //
+        } else if (c.nodeType == 1) {
+
+            //
+            //  Create property for each node name.
+            //
+            if (typeof o[c.nodeName] == 'undefined')
+                o[c.nodeName] = [];
+            o[c.nodeName][o[c.nodeName].length] = getDocumentElement(e, c);
+
+        }
+
+        //
+        //  ...Next!
+        //
+        c = c.nextSibling;
+
+    }
+
+    //
+    //  Now, attributes!
+    //
+    var a = data.attributes;
+    var i = 0;
+    if (a) {
+        for (var i = 0; i < a.length; i ++) {
+            o[a[i].name] = a[i].value;
+        }
+    }
+
+    //
+    //  Clean the text
+    //
+    if (o._text.match(/^\\s*$/))
+        delete o._text;
+
+    //
+    //  Finished!
+    //
+    return o;
+
+}
+    var _fnc = {
+        setListener:function(){
+            $('body').on('http:response', getDocumentElement)
+        },
+        test:function(){
+            _fnc.setListener();
+            util.fnc.httpSend({
+                url:'data/affiliations.xml',
+                selector:'body',
+                type:'xml'
+            });
+           }
+    }
+    return{fnc:_fnc}
+});
---
+++ scripts/util/util.js
@@ -0,0 +1,117 @@
+define(['jQuery'], function($, undefined){
+    'use strict';
+    var w = window, d = document;    
+    var TAG_NAME_DOC_FIRST_CHILD = 'page'; // private member
+    var TAG_NAME_DOC_TITLE = 'title'; // private member
+    
+
+    var _fnc = {
+        noCache:function(){
+            return new Date().getMilliseconds();
+        },
+        httpSend:function(options){
+            var headers = options.hashHeaders || '';
+            var url = options.url;
+            var req = $.ajax({
+                url:url,
+                method:'POST',
+                headers:headers,
+                dataType:options.type || 'text',
+                success:function(data){
+                     $(options.selector).triggerHandler('http:response', [data]); // data is the HTTP Response
+                }                
+            });
+        },
+        appendFragment:function(options){
+            var strText = options.strText;
+            var strIdNodeExists = options.strIdNodeExists;
+            var nodeExist = d.getElementById(strIdNodeExists);
+            var nodeNew = d.createElement('div');
+            var nodeText = d.createTextNode(strText);
+            var strAttribute = nodeExist.getAttribute('class');
+            strAttribute= strAttribute.replace('hide', '');
+            nodeNew.appendChild(nodeText);
+            nodeExist.appendChild(nodeNew);
+            nodeExist.setAttribute('class', strAttribute);    
+        },
+        getElementNode:function(paramNode, paramArray, paramIndex){
+            var node = paramNode;
+            
+            var ELEMENT = 1;
+            var arry = paramArray;
+            var strNameTitle = '';
+            var strValueTitle = '';
+            var strName = '';
+            var strNodeName = '';
+            var strNodeValue = '';
+            var strValue = '';       
+
+            if(node.childNodes.length > 0){
+                   paramArray.push(node.childNodes);
+            }
+        },
+        setAttributes:function(paramHashElementNodes, paramElm){
+            var hash = paramHashElementNodes;
+            var elm = paramElm;
+            for(var i = 0, len = elm.attributes.length; i < len; i++){
+                hash[elm.attributes[i].name] = elm.attributes[i].value;
+            }
+            return hash;
+        },
+        parseXmlToJson:function(paramXml, paramOptionTags){
+            var $xmlDoc = $(paramXml);
+            var arry = [];
+            var arryElementNodes = [];
+            var hashElementNodes = {};
+            var selectorPageTitle = TAG_NAME_DOC_FIRST_CHILD + ' ' + TAG_NAME_DOC_TITLE;
+            var nodeTitle = $xmlDoc.find(selectorPageTitle)[0];
+            var title = $xmlDoc.find(selectorPageTitle)[0].innerHTML;
+            var tagNameContainer = paramOptionTags.childContainerTag;
+            var tagNameFirstChild = paramOptionTags.firstChildTag;
+            var tagsXmlChildsCommon = paramOptionTags.tagsXmlChildsCommon;
+            var selector = tagNameContainer + ' ' + tagNameFirstChild;
+
+            var members = $xmlDoc.find(selector);
+            var hashNodeAttributes = {};
+            var nodeTagName = '';
+            var strAttribute = '';
+            var strAttributeName = '';
+            var strAttributeValue = '';
+            var strNodeValue = null;
+            var strNodeName = null;
+            var hashNodeName = {};
+            var nodeValue = '';
+            var intXmlCategoryCounter = 0; 
+    
+            hashElementNodes['strNameTitle'] = title;
+            hashElementNodes = _fnc.setAttributes(hashElementNodes, nodeTitle);
+
+            for(var i = 0, len = members.length; i < len; i++){ // push each XML common parent nodes to arry
+                arry.push(members[i]);
+            } 
+            
+            for(var i = 0, len = arry.length; i < len; i++){ // access each common parent node and push attributes to hash
+               $(arry[i].childNodes).each(function(index, elm){
+                    if(elm.nodeType == 1){
+
+                      if(elm.hasAttributes()){
+                        hashElementNodes = _fnc.setAttributes(hashElementNodes, elm);
+                      }
+                      strNodeValue = elm.firstChild.nodeValue;
+                      strNodeName = elm.nodeName;
+                      hashElementNodes[elm.nodeName] = strNodeValue;
+                      hashElementNodes['strNodeName'] = strNodeName;
+                    } 
+               });
+               intXmlCategoryCounter = 0; // reset to beginning of tagsXmlChildsCommon array                   
+               arryElementNodes.push(hashElementNodes); // forming hash for underscore template
+               hashElementNodes = new Object; // hashes are reference vars, so, clear it. We do not overwrite previous values
+            } // End for  
+
+            return {pageTitle:title, pageData:arryElementNodes, hashNodeClass:hashNodeAttributes};
+        } // End parseXmlToJson
+    };
+    return{
+        fnc:_fnc
+    }
+});
