f6d547d35a8e9f565e984024c5a047c7140df200
jvmqueue <wheels0090@yahoo.com>
2016-02-15 18:41:23 -0500

    define getData AMD and util AMD

--- scripts/config.js
+++ scripts/config.js
@@ -1,9 +1,13 @@
 requirejs.config({
     paths:{ // only load if AMD defines the alias as a dependency        
+        "getData":["http/getData"],
+        "util":["util/util"],
         "jQuery":["../bower_components/jquery/dist/jquery.min"]
     },
     shim:{
-
+        'jQuery':{
+            exports:'$'
+        }
     },
     deps:['main'] // where our program begins execution
 });
